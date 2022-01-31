715cf781940296b44646af81fb601ea7a12fbf63
Christopher Glinka <glinkachris@gmail.com>
2022-01-31 15:46:17 -0700

    Setup
    Getting started to learn React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+//Components start as a single node
+//fragment = <></> 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+        <header className="page-header">
+        <h1>CraKit React Starter Kit</h1>
       </header>
-    </div>
+      <main>
+
+      </main>
+    </>
   );
 }
 
